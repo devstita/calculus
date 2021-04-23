b233e403d068c9aed9b98e91e78724b8c196c964
Devstita <devstita@gmail.com>
2021-04-23 16:26:48 +0900

    Developing: Graph Interface

--- graph.py
+++ graph.py
@@ -1,20 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(-5, 5, 100)
-y  = x ** x
+if __name__ == '__main__': 
+    x = np.linspace(-5, 5, 100000)
+    y  = x ** 2
 
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-ax.spines['left'].set_position('center')
-ax.spines['bottom'].set_position('center')
-ax.spines['right'].set_color('none')
-ax.spines['top'].set_color('none')
-ax.xaxis.set_ticks_position('bottom')
-ax.yaxis.set_ticks_position('left')
-
-# plot the function
-plt.plot(x,y, 'g')
-
-# show the plot
-plt.show()
+    plt.plot(x, y)
+    plt.show()
